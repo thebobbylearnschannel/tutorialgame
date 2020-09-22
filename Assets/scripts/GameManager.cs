34e2a5c3a8e459bdc687559dfdefc6eebaab95d3
Normal User <Normal User@Computer3.edu>
2020-09-21 20:00:25 -0400

    Initial Commit

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+
+    public float restartDelay = 2f;
+
+    public GameObject completeLevelUI;
+
+    public void CompleteLevelOne()
+    {
+        Debug.Log("Level 1 Complete");
+        completeLevelUI.SetActive(true);
+
+    }
+    public void EndGame()
+    {
+        if (gameHasEnded == false)
+        {
+            Debug.Log("Game Over");
+            gameHasEnded = true;
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
